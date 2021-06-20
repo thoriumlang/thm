f1e34dd4fe312152aee7af17ac8e086e3ee90a1c
Christophe Pollet <cpollet@users.noreply.github.com>
2021-06-20 11:58:24 +0200

    Remove test_fibonacci from vm

--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -489,33 +489,4 @@ mod tests {
         assert_eq!(false, vm.flags.zero);
         assert_eq!(true, vm.flags.negative);
     }
-
-    #[test]
-    fn test_fibonacci() {
-        let mut vm = VM::new();
-
-        // fibonacci(5)
-        vm.program = vec![
-            /*0*/  Op::LOAD.bytecode(), 0x00, 0x00, 0x00, 0x00, 0x05, // r0 = 5
-            /*6*/  Op::LOAD.bytecode(), 0x01, 0x00, 0x00, 0x00, 0x00, // r1 = 0
-            /*12*/ Op::LOAD.bytecode(), 0x02, 0x00, 0x00, 0x00, 0x01, // r2 = 1
-
-            /*18*/ Op::LOAD.bytecode(), 0x03, 0x00, 0x00, 0x00, 0x00, // r3 = 0
-            /*24*/ Op::LOAD.bytecode(), 0x04, 0x00, 0x00, 0x00, 0x01, // r4 = 1
-
-            /*30*/ Op::CMP.bytecode(), 0x00, 0x01,                    // r0 == r1?
-            /*33*/ Op::JE.bytecode(), 0x00, 0x00, 0x00, 0x37,         // if r0 == r1 -> jump 55
-
-            /*38*/ Op::MOV.bytecode(), 0x05, 0x03,                    // r5 = r3
-            /*41*/ Op::MOV.bytecode(), 0x03, 0x04,                    // r3 = r4
-            /*44*/ Op::ADD.bytecode(), 0x04, 0x05,                    // r4 += r5
-            /*47*/ Op::ADD.bytecode(), 0x01, 0x02,                    // r1 += r2
-            /*50*/ Op::JMP.bytecode(), 0x00, 0x00, 0x00, 0x1E,        // jump 30
-
-            /*55*/ Op::HALT.bytecode()
-        ];
-        vm.run();
-
-        assert_eq!(vm.registers[3], 5);
-    }
 }
