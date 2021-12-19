d499e712b144bd3179935cc3c64d7cc2f1c685c1
Christophe Pollet <cpollet@users.noreply.github.com>
2021-12-19 21:21:02 +0100

    Use latest commit from MiniFB

--- src/vm/cmake/FetchMiniFB.cmake
+++ src/vm/cmake/FetchMiniFB.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         minifb
         GIT_REPOSITORY https://github.com/emoon/minifb.git
-        GIT_TAG        f54b94a
+        GIT_TAG        af15719
         GIT_SHALLOW    1
 )
 
